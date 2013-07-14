1e67b7a601c825837862e780deb4e90f094c9823
andytuba <andytuba@andytuba.com>
2013-07-13 19:04:08 -0500

    try to send unsubscribe request to reddit api

--- background.js
+++ background.js
@@ -1,14 +1,35 @@
 
 var uh = '';
 var subredditRE = /^https?:\/\/(?:[a-z]+).reddit.com\/r\/([\w\.\+]+)\//i
+var subscribeApiEndpoint = 'http://reddit.com/api/subscribe';
 
-function unsubscribe(info)
+function onclickUnsubscribe(info)
 {
  	var match = info.linkUrl.match(subredditRE);
 	var subreddit = match && match[1];
 	if (typeof subreddit === "undefined") console.log("nothing to do", info.linkUrl);
 
 	console.log("unsubscribe from " + subreddit, "given", info);
+	unsubscribe(subreddit);	
+}
+
+function unsubscribe(subreddit) {
+	var params = {
+		'r': subreddit,
+		'action': 'unsub',
+		'uh': uh,
+	};
+	var items = [];
+	for (var key in params) {
+		if (!params.hasOwnProperty(key)) continue;
+		var item = key + '=' + params[key];
+		items.push(item);
+	}
+	var data = items.join('&');
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", subscribeApiEndpoint);
+	xhr.send(JSON.stringify(data))
 
 }
 
@@ -29,7 +50,7 @@ chrome.contextMenus.create({
 		"*://reddit.com/r/*",
 		"*://*.reddit.com/r/*"
 		],
-	onclick: unsubscribe
+	onclick: onclickUnsubscribe
 });
 
 
